343c8237867ac2a17d9c6e1ad8f772b24c696fe0
julesjulicher2 <38348256+julesjulicher2@users.noreply.github.com>
2019-01-05 14:30:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -265,15 +265,13 @@ async def announce(ctx, message, everyone):
             embed = discord.Embed(description=f"`{message}`", colour=0xff0000)
             embed.set_author(name=f"Announcement")
             embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/520988858700005386/8170a9c2e6ddd51555f7dacc78faff83.png?size=128")
-            embed.set_footer(text=f"By: `{author}`", icon_url=writer.avatar_url)
-            await bot.send_message(channel, embed=embed)
-            
+            embed.set_footer(text=f"By: `{writer}`", icon_url=writer.avatar_url)
             await bot.send_message(bot.get_channel("291936456794963968"), "@everyone")
             await bot.send_message(bot.get_channel("291936456794963968"), embed=embed)
         elif everyone == "no":
             bot.send_message(channel, "You have to say yes or no at the end.")
-    else:
-        await bot.send_message(channel, "geen toegang")
-        message = ctx.message.author
+        else:
+            await bot.send_message(channel, "geen toegang")
+            message = ctx.message.author
 	
 bot.run(os.environ.get('TOKEN'))

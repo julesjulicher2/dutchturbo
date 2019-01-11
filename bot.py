fd3bd6da94cb6782726f141e7efed849576e034e
julesjulicher2 <38348256+julesjulicher2@users.noreply.github.com>
2019-01-11 22:55:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_ready():
     await loop()
 @bot.event
 async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name = ("🙊gasten🙊")
+    role = discord.utils.get(member.server.roles, name = "🙊gasten🙊")
     await bot.add_roles(member, role)
 #------------------------------------------------------
 #ids
